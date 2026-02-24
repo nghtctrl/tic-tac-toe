621d91fcbec1a57372e94bf16a2b0934a47dd31b
Jiho Kim <55632840+nghtctrl@users.noreply.github.com>
2026-02-24 15:07:57 -0600

    Complete tutorial

---
+++ src/types.d.ts
@@ -0,0 +1,1 @@
+type Squares = (string | null)[];
---
+++ src/utils/utils.ts
@@ -0,0 +1,34 @@
+export function isDraw(squares: Squares): boolean {
+  for (let i = 0; i < squares.length; i++) {
+    if (squares[i] === null) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export function calculateWinner(squares: Squares): string | null {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (
+      squares[a] !== null &&
+      squares[a] === squares[b] &&
+      squares[a] === squares[c]
+    ) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
